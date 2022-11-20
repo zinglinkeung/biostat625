074e19351b428af5786dd18109dac9a7d46d020d
Zhilin Zhang <zzhilin@umich.edu>
2022-11-19 21:02:25 -0500

    added vignette

--- R/t1.R
+++ R/t1.R
@@ -1,1 +1,16 @@
-print(1)
+#'Square
+#'
+#'Gets the square of a number
+#'
+#'@param x input value
+#'
+#'@return the square of x
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+square = function(x) {
+  return (x^2)
+}
